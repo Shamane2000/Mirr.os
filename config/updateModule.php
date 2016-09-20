39784a3c20cb43d5b9136dc81bc6a7d20e191431
Tobias Grasse <mail@tobias-grasse.net>
2016-09-20 16:04:27 +0200

    Implements updating functionality for modules
    Load the new module from the server and replace the old one with it.
    
    Signed-off-by: Tobias Grasse <mail@tobias-grasse.net>

---
+++ config/updateModule.php
@@ -0,0 +1,32 @@
+<?php
+include('glancrConfig.php');
+
+$orderZip = file_get_contents($apibaseurl . '/update/zipModule.php?module=' . $_POST['name']);
+$file = $apibaseurl . '/update/moduleZips/' . $_POST['name'] . '-' . $_POST['version'] . '.zip';
+$tmpFile = $basedir . '/tmp/tmp_file.zip';
+
+if (!copy($file, $tmpFile)) {
+    http_response_code(500);
+    exit("failed to create $tmpFile from $file...\n");
+}
+
+$zip = new ZipArchive();
+if ($zip->open($tmpFile, ZIPARCHIVE::CREATE)) {
+	$zip->extractTo($basedir .'/modules/');
+
+    //@TODO This can be removed; PHP correctly sets 0755 for directories and 0644 for files
+//	$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($basedir .'/modules/' . $_FILES['moduleZip']['name']));
+//
+//	foreach($iterator as $item) {
+//		chmod($item, 0777);
+//	}
+
+	$zip->close();
+	http_response_code(200);
+} else {
+    http_response_code(500);
+}
+
+unlink($tmpFile);
+setConfigValue('reload', '1');
+?>
