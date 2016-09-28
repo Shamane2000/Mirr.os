2405719e48e60f6852fcfa3ce9859aa998e32637
Tobias Grasse <mail@tobias-grasse.net>
2016-09-28 22:17:04 +0200

    Refactor update endpoint for backend JS
    Now only a base script for class instantiation
    
    Signed-off-by: Tobias Grasse <mail@tobias-grasse.net>

--- config/runUpdate.php
+++ config/runUpdate.php
@@ -1,31 +1,25 @@
 <?php
+
+require_once '../vendor/autoload.php';
 include('glancrConfig.php');
 
-$orderZip = file_get_contents(getApiBaseUrl() . '/update/zipModule.php?module=' . $_POST['name']);
-$file = getApiBaseUrl() . '/update/moduleZips/' . $_POST['name'] . '-' . $_POST['version'] . '.zip';
-$tmpFile = GLANCR_ROOT . '/tmp/tmp_file.zip';
 
-if (!copy($file, $tmpFile)) {
-    http_response_code(500);
-    exit("failed to create $tmpFile from $file...\n");
+switch ($_POST['type']) {
+    case 'system':
+        $update = new \glancr\SystemUpdater(getSystemInfo());
+        $result = $update->updateSystem();
+        return $result;
+        break;
+    case 'modules':
+        $update = new \glancr\ModuleUpdater();
+        $result = $update->updateModule($_POST['name'], $_POST['version']);
+        if($result === true) {
+            http_response_code(200);
+        } else {
+            http_response_code(500);
+        }
+        break;
+    default:
+        return false;
+
 }
-
-$zip = new ZipArchive();
-if ($zip->open($tmpFile, ZIPARCHIVE::CREATE)) {
-	$zip->extractTo(GLANCR_ROOT .'/modules/');
-
-    //@TODO This can be removed; PHP correctly sets 0755 for directories and 0644 for files
-//	$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($basedir .'/modules/' . $_FILES['moduleZip']['name']));
-//
-//	foreach($iterator as $item) {
-//		chmod($item, 0777);
-//	}
-
-	$zip->close();
-	http_response_code(200);
-} else {
-    http_response_code(500);
-}
-
-unlink($tmpFile);
-setConfigValue('reload', '1');
