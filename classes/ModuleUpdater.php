a9b44564dfbfb26cbd99501c51e79e9fa4916cc0
Tobias Grasse <mail@tobias-grasse.net>
2016-10-04 19:17:04 +0200

    Refactors ModuleUpdate update check to use info.json
    Signed-off-by: Tobias Grasse <mail@tobias-grasse.net>

--- classes/ModuleUpdater.php
+++ classes/ModuleUpdater.php
@@ -1,14 +1,10 @@
 <?php
-
 namespace glancr;
 use ZipArchive;
-
 class ModuleUpdater
 {
-
     function __construct() {
     }
-
     /**
      * Checks the glancr API if all installed modules are up-to-date.
      */
@@ -16,7 +12,6 @@ class ModuleUpdater
         $modules_content = scandir(GLANCR_ROOT .'/modules');
         $modules_available = [];
         $updates_available = [];
-
         foreach($modules_content as $file) {
             if(is_dir(GLANCR_ROOT .'/modules/' . $file) && $file != '.' && $file != '..') {
                 $modules_available[] = $file;
@@ -24,27 +19,24 @@ class ModuleUpdater
         }
         $serverVersions = json_decode(file_get_contents(GLANCR_API_BASE .'/update/getVersions.php'), true);
         $onServer = array_keys($serverVersions);
-
-
         foreach($modules_available as $module_available) {
-            $versionFile = file_get_contents(GLANCR_ROOT . '/modules/' . $module_available . '/version.txt');
+            $infoFile = file_get_contents(GLANCR_ROOT . '/modules/' . $module_available . '/info.json');
+            $infoObject = json_decode($infoFile);
+            $localVersion = $infoObject['module']['version'];
 
             if (array_key_exists($module_available, $serverVersions)) {
                 unset($onServer[array_search($module_available, $onServer)]);
-
-                if ($versionFile < $serverVersions[$module_available]) {
+                if ($localVersion < $serverVersions[$module_available]) {
                     $updates_available[] = [
                         'name' => $module_available,
-                        'oldVersion' => $versionFile,
+                        'oldVersion' => $localVersion,
                         'newVersion' => $serverVersions[$module_available]
                     ];
                 }
             }
         }
-
         if (!empty($updates_available)) {
             error_log("Module updates available: " . print_r($updates_available, true));
-
             if (unserialize(getConfigValue('module_updates')) === $updates_available) {
                 return;
             } else {
@@ -55,7 +47,6 @@ class ModuleUpdater
             }
         }
     }
-
     /**
      * @param string $name A valid Glancr module name, all lowercased. Must exist on the server.
      * @param string $version A correct version number for the given module.
@@ -65,26 +56,20 @@ class ModuleUpdater
         // Generate the module ZIP file from the latest source.
         //@TODO: Refactor this with Guzzle/cURL or similar.
         file_get_contents(GLANCR_API_BASE . '/update/zipModule.php?module=' . $name);
-
         $file = GLANCR_API_BASE . '/update/moduleZips/' . $name . '-' . $version . '.zip';
         $tmpFile = GLANCR_ROOT . '/tmp/tmp_file.zip';
-
         $result = false;
-
         if (!copy($file, $tmpFile)) {
             error_log("failed to create $tmpFile from $file...\n");
         }
-
         $zip = new ZipArchive();
         if ($zip->open($tmpFile, ZIPARCHIVE::CREATE)) {
             $zip->extractTo(GLANCR_ROOT .'/modules/');
             $zip->close();
             $result = true;
         }
-
         unlink($tmpFile);
         setConfigValue('reload', '1');
-
         return $result;
     }
 }
